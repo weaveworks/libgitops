6c1ab8369121dbc06830ad8f6f333a496183d96f
Lucas Käldström <lucas@weave.works>
2020-07-16 15:19:05 +0300

    Add a set of small Frame utils

---
+++ pkg/serializer/frame_utils.go
@@ -0,0 +1,35 @@
+package serializer
+
+import "io"
+
+// FrameList is a list of frames (byte arrays), used for convenience functions
+type FrameList [][]byte
+
+// ReadFrameList is a convenience method that reads all available frames from the FrameReader
+// into a returned FrameList
+func ReadFrameList(fr FrameReader) (FrameList, error) {
+	frameList := [][]byte{}
+	for {
+		// Read until we get io.EOF or an error
+		frame, err := fr.ReadFrame()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		// Append all frames to the returned list
+		frameList = append(frameList, frame)
+	}
+	return frameList, nil
+}
+
+// WriteFrameList is a convenience method that writes a set of frames to a FrameWriter
+func WriteFrameList(fw FrameWriter, frameList FrameList) error {
+	// Loop all frames in the list, and write them individually to the FrameWriter
+	for _, frame := range frameList {
+		if _, err := fw.Write(frame); err != nil {
+			return err
+		}
+	}
+	return nil
+}
