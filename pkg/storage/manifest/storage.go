6af3864a0c85c2b1b795e54398266264dc726549
Lucas Käldström <lucas@weave.works>
2020-08-14 16:55:34 +0300

    Remove the twowaymanifest storage helper, should be in ignite instead

--- pkg/storage/manifest/storage.go
+++ pkg/storage/manifest/storage.go
@@ -6,7 +6,6 @@ import (
 	"github.com/weaveworks/libgitops/pkg/storage"
 	"github.com/weaveworks/libgitops/pkg/storage/sync"
 	"github.com/weaveworks/libgitops/pkg/storage/watch"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 // NewManifestStorage constructs a new storage that watches unstructured manifests in the specified directory,
@@ -24,28 +23,6 @@ func NewManifestStorage(manifestDir string, ser serializer.Serializer) (*Manifes
 	}
 
 	ss := sync.NewSyncStorage(ws)
-
-	return &ManifestStorage{
-		Storage: ss,
-	}, nil
-}
-
-// NewManifestStorage constructs a new storage that watches unstructured manifests in the specified directory,
-// decodable using the given serializer. However, all changes in the manifest directory, are also propagated to
-// the structured data directory that's backed by the default storage implementation. Writes to this storage are
-// propagated to both the manifest directory, and the data directory. The GroupVersion used for the GenericRawStorage
-// must be specified. The IdentifierFactories also need to be specified.
-// TODO: Maybe move this to Ignite?
-func NewTwoWayManifestStorage(manifestDir, dataDir string, ser serializer.Serializer, gv schema.GroupVersion, identifiers []runtime.IdentifierFactory) (*ManifestStorage, error) {
-	ws, err := watch.NewGenericWatchStorage(storage.NewGenericStorage(storage.NewGenericMappedRawStorage(manifestDir), ser, identifiers))
-	if err != nil {
-		return nil, err
-	}
-
-	ss := sync.NewSyncStorage(
-		storage.NewGenericStorage(
-			storage.NewGenericRawStorage(dataDir, gv, serializer.ContentTypeJSON), ser, identifiers),
-		ws)
 
 	return &ManifestStorage{
 		Storage: ss,
