54c35df195a48a694d0500c0bd47eca542ba27c1
Lucas Käldström <lucas@weave.works>
2020-08-14 14:18:57 +0300

    Refactor main.go, now use the Git backend too

--- cmd/sample-app/main.go
+++ cmd/sample-app/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"os"
@@ -14,48 +15,82 @@ import (
 	"github.com/weaveworks/libgitops/cmd/sample-app/version"
 
 	"github.com/labstack/echo"
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/sirupsen/logrus"
 	"github.com/weaveworks/libgitops/cmd/sample-app/apis/sample/scheme"
 	"github.com/weaveworks/libgitops/cmd/sample-app/apis/sample/v1alpha1"
+	"github.com/weaveworks/libgitops/pkg/gitdir"
 	"github.com/weaveworks/libgitops/pkg/logs"
 	"github.com/weaveworks/libgitops/pkg/runtime"
 	"github.com/weaveworks/libgitops/pkg/serializer"
 	"github.com/weaveworks/libgitops/pkg/storage"
+	"github.com/weaveworks/libgitops/pkg/storage/transaction"
 )
 
 var (
 	carGVK = v1alpha1.SchemeGroupVersion.WithKind("Car")
+
+	identityFlag = pflag.String("identity-file", "", "Where the SSH private key is")
 )
 
-const ManifestDir = "/tmp/libgitops/manifest"
+const (
+	ManifestDir = "/tmp/libgitops/manifest"
+	sshKnownHostsFile = "~/.ssh/known_hosts"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
 	// Parse the version flag
 	parseVersionFlag()
 
 	// Run the application
-	if err := run(); err != nil {
+	if err := run(*identityFlag); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-func run() error {
+func expandAndRead(filePath string) ([]byte, error) {
+	expandedPath, err := homedir.Expand(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadFile(expandedPath)
+}
+
+func run(identityFile string) error {
+	if len(identityFile) == 0 {
+		return fmt.Errorf("--identity-file is required")
+	}
+
+	identityContent, err := expandAndRead(identityFile)
+	if err != nil {
+		return err
+	}
+
+	knownHostsContent, err := expandAndRead(sshKnownHostsFile)
+	if err != nil {
+		return err
+	}
+
 	// Construct the GitDirectory implementation which backs the storage
-	/*gitDir, err := gitdir.NewGitDirectory("https://github.com/luxas/ignite-gitops", gitdir.GitDirectoryOptions{
+	gitDir, err := gitdir.NewGitDirectory("git@github.com:luxas/ignite-gitops.git", gitdir.GitDirectoryOptions{
 		Branch:   "master",
 		Interval: 10 * time.Second,
+		IdentityFileContent: identityContent,
+		KnownHostsFileContent: knownHostsContent,
 	})
 	if err != nil {
 		return err
 	}
 
-	// Wait for the repo to be cloned
-	if err := gitDir.WaitForClone(); err != nil {
+	gitStorage, err := transaction.NewGitStorage(gitDir, scheme.Serializer)
+	if err != nil {
 		return err
-	}*/
+	}
 
 	// Create the manifest directory
 	if err := os.MkdirAll(ManifestDir, 0755); err != nil {
@@ -63,21 +98,15 @@ func run() error {
 	}
 
 	// Set the log level
-	logs.Logger.SetLevel(logrus.DebugLevel)
+	logs.Logger.SetLevel(logrus.TraceLevel)
 
-	// Set up the ManifestStorage
-	/*ms, err := manifest.NewManifestStorage(ManifestDir, scheme.Serializer)
-	if err != nil {
-		return err
-	}*/
-	ms := storage.NewGenericStorage(
+	plainStorage := storage.NewGenericStorage(
 		storage.NewGenericRawStorage(ManifestDir, v1alpha1.SchemeGroupVersion, serializer.ContentTypeYAML),
 		scheme.Serializer,
 		[]runtime.IdentifierFactory{runtime.Metav1NameIdentifier},
 	)
 
-	defer func() { _ = ms.Close() }()
-	//Client := client.NewClient(cache.NewCache(ms))
+	defer func() { _ = plainStorage.Close() }()
 
 	// Set up the echo server
 	e := echo.New()
@@ -86,14 +115,14 @@ func run() error {
 		return c.String(http.StatusOK, "Welcome!")
 	})
 
-	e.GET("/:name", func(c echo.Context) error {
+	e.GET("/plain/:name", func(c echo.Context) error {
 		name := c.Param("name")
 		if len(name) == 0 {
 			return echo.NewHTTPError(http.StatusBadRequest, "Please set name")
 		}
 
 		objKey := storage.NewObjectKey(storage.NewKindKey(carGVK), runtime.NewIdentifier("default/"+name))
-		obj, err := ms.Get(objKey)
+		obj, err := plainStorage.Get(objKey)
 		if err != nil {
 			return err
 		}
@@ -104,7 +133,7 @@ func run() error {
 		return c.JSONBlob(http.StatusOK, content.Bytes())
 	})
 
-	e.POST("/:name", func(c echo.Context) error {
+	e.POST("/plain/:name", func(c echo.Context) error {
 		name := c.Param("name")
 		if len(name) == 0 {
 			return echo.NewHTTPError(http.StatusBadRequest, "Please set name")
@@ -116,10 +145,54 @@ func run() error {
 		obj.Namespace = "default"
 		obj.Spec.Brand = fmt.Sprintf("Acura-%03d", rand.Intn(1000))
 
-		err := ms.Set(obj)
+		err := plainStorage.Set(obj)
 		if err != nil {
 			return err
 		}
+		return c.String(200, "OK!")
+	})
+
+	e.GET("/git/", func(c echo.Context) error {
+		objs, err := gitStorage.List(storage.NewKindKey(carGVK))
+		if err != nil {
+			return err
+		}
+		/*var content bytes.Buffer
+		if err := scheme.Serializer.Encoder().Encode(serializer.NewJSONFrameWriter(&content), ); err != nil {
+			return err
+		}*/
+		return c.JSON(http.StatusOK, objs)
+	})
+
+	e.POST("/git/:name", func(c echo.Context) error {
+		name := c.Param("name")
+		if len(name) == 0 {
+			return echo.NewHTTPError(http.StatusBadRequest, "Please set name")
+		}
+
+		objKey := storage.NewObjectKey(storage.NewKindKey(carGVK), runtime.NewIdentifier("default/"+name))
+		err := gitStorage.Transaction(context.Background(), "foo-branch", func(ctx context.Context, s storage.Storage) (*transaction.CommitSpec, error) {
+			obj, err := s.Get(objKey)
+			if err != nil {
+				return nil, err
+			}
+			car := obj.(*v1alpha1.Car)
+			car.Status.Distance = rand.Uint64()
+			car.Status.Speed = rand.Float64() * 100
+			if err := s.Set(car); err != nil {
+				return nil, err
+			}
+
+			return &transaction.CommitSpec{
+				AuthorName: stringVar("Lucas Käldström"),
+				AuthorEmail: stringVar("lucas.kaldstrom@hotmail.co.uk"),
+				Message: "Just updating some car status :)",
+			},nil
+		})
+		if err != nil {
+			return err
+		}
+
 		return c.String(200, "OK!")
 	})
 
@@ -150,6 +223,10 @@ func parseVersionFlag() {
 		fmt.Printf("sample-app version: %#v\n", version.Get())
 		os.Exit(0)
 	}
+}
+
+func stringVar(s string) *string {
+	return &s
 }
 
 /*
