53be24cd9c034aa442e2ff403e7f7b23f1a5a08a
Lucas Käldström <lucas@weave.works>
2021-01-18 23:51:05 +0200

    Add a definition and sample implementation of PathExcluder.

---
+++ pkg/storage/core/path_excluder.go
@@ -0,0 +1,43 @@
+package core
+
+import (
+	"context"
+	"path/filepath"
+)
+
+// PathExcluder is an interface that lets the user implement custom policies
+// for whether a given relative path to a given directory (fs is scoped at
+// that directory) should be considered for an operation (e.g. inotify watch
+// or file search).
+type PathExcluder interface {
+	// ShouldExcludePath takes in a context, the fs filesystem abstraction,
+	// and a relative path to the file which should be determined if it should
+	// be excluded or not.
+	ShouldExcludePath(ctx context.Context, fs AferoContext, path string) bool
+}
+
+// ExcludeGitDirectory implements PathExcluder.
+var _ PathExcluder = ExcludeGitDirectory{}
+
+// ExcludeGitDirectory is a sample implementation of PathExcluder, that excludes
+// all files under a ".git" directory, anywhere in the tree under the root directory.
+type ExcludeGitDirectory struct{}
+
+func (ExcludeGitDirectory) ShouldExcludePath(_ context.Context, _ AferoContext, path string) bool {
+	// Always start from a clean path
+	path = filepath.Clean(path)
+	for {
+		// get the current base entry name
+		baseName := filepath.Base(path)
+		// This means path is now an empty string; we did not find a .git directory anywhere
+		if baseName == "." {
+			return false
+		}
+		// We possibly found a directory named git; this path should be excluded
+		if baseName == ".git" {
+			return true
+		}
+		// "go up" one directory for the next iteration
+		path = filepath.Dir(path)
+	}
+}
---
+++ pkg/storage/core/path_excluder_test.go
@@ -0,0 +1,69 @@
+package core
+
+import (
+	"context"
+	"testing"
+)
+
+func TestExcludeGitDirectory_ShouldExcludePath(t *testing.T) {
+	tests := []struct {
+		name string
+		path string
+		want bool
+	}{
+		{
+			name: "normal",
+			path: ".git",
+			want: true,
+		},
+		{
+			name: "with relative path",
+			path: "./.git",
+			want: true,
+		},
+		{
+			name: "with many parents",
+			path: "/foo/bar/.git",
+			want: true,
+		},
+		{
+			name: "with many children",
+			path: ".git/foo/bar/baz",
+			want: true,
+		},
+		{
+			name: "with parents and children",
+			path: "./foo/bar/.git/baz/bar",
+			want: true,
+		},
+		{
+			name: "empty",
+			path: "",
+			want: false,
+		},
+		{
+			name: "local dir",
+			path: ".",
+			want: false,
+		},
+		{
+			name: "other prefix",
+			path: "foo.git",
+			want: false,
+		},
+		{
+			name: "other suffix",
+			path: ".gitea",
+			want: false,
+		},
+	}
+	e := ExcludeGitDirectory{}
+	ctx := context.Background()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := e.ShouldExcludePath(ctx, nil, tt.path); got != tt.want {
+				t.Errorf("ExcludeGitDirectory.ShouldExcludePath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
